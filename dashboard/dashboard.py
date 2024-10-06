3c20054992c8eb7b4bca681b3d3ab3f6b4b38b85
FransiscusSuhargo <s160422070@student.ubaya.ac.id>
2024-10-06 11:15:14 +0700

    Finish

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -41,14 +41,25 @@ def customer_counts(df):
     customer_count = df.groupby(by="customer_city").count()['customer_unique_id'].sort_values(ascending=False).head(20)
     return customer_count
 
-def explore_categories_city(df):
-    category_highest = df.groupby(['customer_city', 'product_category_name']).agg({
+def top_3_categories_city(df):
+    category_highest = df.groupby(['customer_city', 'product_category_name_english']).agg({
         'product_id': 'count'  
     }).rename(columns={'product_id': 'count'}).reset_index()
     top_cities = category_highest.groupby('customer_city')['count'].sum().nlargest(10).index
     top_categories_per_city = category_highest[category_highest['customer_city'].isin(top_cities)]
     top_3_categories_per_city = top_categories_per_city.groupby('customer_city').apply(lambda x: x.nlargest(3, 'count')).reset_index(drop=True)
     return(top_3_categories_per_city)
+def categories_product_sales(df):
+    # Mencari tahu kategori produk apa yang paling banyak dijual
+    category_highest = df.groupby('product_category_name_english').agg({
+    'product_id': 'count'  
+    }).rename(columns={'product_id': 'count'}).sort_values(by='count', ascending=False)
+
+    # Mencari tahu kategori produk apa yang paling sedikit dijual
+    category_lowest = df.groupby('product_category_name_english').agg({
+    'product_id': 'count'  
+    }).rename(columns={'product_id': 'count'}).sort_values(by='count', ascending=True)
+    return category_highest, category_lowest
 
 # Load cleaned data
 all_df = pd.read_csv("dashboard/all_data.csv")
@@ -84,7 +95,8 @@ sales_over_time_data = sales_over_time(main_df)
 average_delivery_time = delivery_time(main_df)
 payment_counts_data = payment_counts(main_df)
 customer_counts_data = customer_counts(main_df)
-explore_categories_city_data = explore_categories_city(main_df)
+explore_categories_city_data = top_3_categories_city(main_df)
+category_highest, category_lowest = categories_product_sales(main_df) 
 
 st.title("Sales Over Time")
 fig, ax = plt.subplots(figsize=(10, 5))
@@ -103,8 +115,8 @@ ax.tick_params(axis='y', labelsize=10)
 st.pyplot(fig)
 
 st.title("Category per Cities")
-category_highest_reset = explore_categories_city_data.reset_index().sort_values(by="count" ascending=False)
-category_lowest_reset = explore_categories_city_data.reset_index().sort_values(by="count" ascending=True)
+category_highest_reset = category_highest.reset_index().sort_values(by="count",ascending=False)
+category_lowest_reset = category_lowest.reset_index().sort_values(by="count",ascending=True)
 
 colors_highest = ["#72BCD4", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
@@ -112,7 +124,7 @@ fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(24, 6))
 
 #Bar plot untuk 5 kategori dengan penjualan terbanyak
 sns.barplot(x=category_highest_reset.head(5)['count'], 
-            y=category_highest_reset.head(5)['product_category_name'], 
+            y=category_highest_reset.head(5)['product_category_name_english'], 
             palette=colors_highest, ax=ax[0])
 ax[0].set_title("Top 5 Most Sold Product Categories", fontsize=15)
 ax[0].set_xlabel("Count")
@@ -121,7 +133,7 @@ ax[0].set_ylabel("Product Category")
 colors_lowest = ["#FF6F61", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 # Bar plot untuk 5 kategori dengan penjualan tersedikit
 sns.barplot(x=category_lowest_reset.head(5)['count'], 
-            y=category_lowest_reset.head(5)['product_category_name'], 
+            y=category_lowest_reset.head(5)['product_category_name_english'], 
             palette=colors_lowest, ax=ax[1])
 ax[1].set_title("Top 5 Least Sold Product Categories", fontsize=15)
 ax[1].set_xlabel("Count")
@@ -139,7 +151,7 @@ top_3_categories_per_city = top_categories_per_city.groupby('customer_city').app
 
 plt.figure(figsize=(12, 6))
 
-sns.barplot(x='customer_city', y='count', hue='product_category_name', data=top_3_categories_per_city)
+sns.barplot(x='customer_city', y='count', hue='product_category_name_english', data=top_3_categories_per_city)
 
 plt.title('Top 3 Product Categories per City')
 plt.xlabel('Customer City')
@@ -171,7 +183,7 @@ ax[0].set_title("Top 5 Fastest Cities by Delivery Time", fontsize=15)
 ax[0].set_xlabel("Average Delivery Time (Hours)")
 ax[0].set_ylabel("State")
 
-average_delivery_time.sort_values(by="delivery_time", ascending=True)
+average_delivery_time = average_delivery_time.sort_values(by="delivery_time", ascending=False )
 sns.barplot(x=average_delivery_time['delivery_time'].head(5), 
             y=average_delivery_time['customer_city'].head(5), 
             palette=colors_slowest, ax=ax[1])
